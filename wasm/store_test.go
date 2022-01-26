ba1c0346eb4f071a306bbd2ed516b1605f4cce70
Takaya Saeki <takaya@tetrate.io>
2022-01-26 22:38:31 +0900

    Implement args_get and args_sizes_get
    Signed-off-by: Takaya Saeki <takaya@tetrate.io>

--- wasm/store_test.go
+++ wasm/store_test.go
@@ -1,6 +1,8 @@
 package wasm
 
 import (
+	"encoding/binary"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -51,4 +53,46 @@ func TestBuildFunctionInstances_FunctionNames(t *testing.T) {
 
 	// We expect unknown for any functions missing data in the NameSection
 	require.Equal(t, []string{"unknown", "two", "unknown", "four", "five"}, names)
+}
+
+func TestValidateAddrRange(t *testing.T) {
+	name := "test"
+	s := NewStore(nil)
+	mi := s.getModuleInstance(name)
+
+	m := &Module{
+		MemorySection: []*LimitsType{{Min: 100}},
+	}
+
+	_, err := s.buildMemoryInstances(m, mi)
+	require.NoError(t, err)
+
+	require.True(t, mi.Memory.ValidateAddrRange(uint32(0), uint64(0)))
+	require.True(t, mi.Memory.ValidateAddrRange(uint32(0), uint64(100*PageSize)))
+	require.False(t, mi.Memory.ValidateAddrRange(uint32(0), uint64(100*PageSize+1)))
+	require.False(t, mi.Memory.ValidateAddrRange(uint32(1), uint64(100*PageSize)))
+	require.False(t, mi.Memory.ValidateAddrRange(uint32(100*PageSize), uint64(0)))
+}
+
+func TestPutUint32(t *testing.T) {
+	name := "test"
+	s := NewStore(nil)
+	mi := s.getModuleInstance(name)
+
+	m := &Module{
+		MemorySection: []*LimitsType{{Min: 100}},
+	}
+
+	_, err := s.buildMemoryInstances(m, mi)
+	require.NoError(t, err)
+
+	maxUint32 := uint32(math.MaxUint32)
+	asymmetryBitsVal := uint32(0xfffffffe)
+	require.True(t, mi.Memory.PutUint32(uint32(0), asymmetryBitsVal))
+	require.Equal(t, asymmetryBitsVal, binary.LittleEndian.Uint32(mi.Memory.Buffer[0:4]))
+	require.True(t, mi.Memory.PutUint32(uint32(0), maxUint32))
+	require.Equal(t, maxUint32, binary.LittleEndian.Uint32(mi.Memory.Buffer[0:4]))
+	require.True(t, mi.Memory.PutUint32(uint32(100*PageSize-4), asymmetryBitsVal))
+	require.Equal(t, asymmetryBitsVal, binary.LittleEndian.Uint32(mi.Memory.Buffer[100*PageSize-4:100*PageSize]))
+	require.False(t, mi.Memory.PutUint32(uint32(100*PageSize-3), asymmetryBitsVal))
 }
